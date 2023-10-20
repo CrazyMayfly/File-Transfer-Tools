3b9fbce9b133be2abad1141eb329dffc81dd1b77
april <aprilblog@163.com>
2023-10-20 13:56:35 +0800

    perf(test): optimized test suite.

--- test/test_fts.py
+++ test/test_fts.py
@@ -1,5 +1,6 @@
 import os.path
 import unittest
+from pathlib import Path
 
 from FTS import FTS, handle_ctrl_event
 from tools import create_random_file
@@ -7,17 +8,17 @@ from tools import create_random_file
 
 class FTSTest(unittest.TestCase):
     def setUp(self):
-        self.test_dir = os.path.normcase(os.path.join('ftt_test', 'fts_test'))
-        if not os.path.exists(self.test_dir):
+        self.test_dir = Path('ftt_test', 'fts_test')
+        if not self.test_dir.exists():
             os.makedirs(self.test_dir)
-        create_random_file(os.path.join(self.test_dir, 'fts_file.txt'))
-        create_random_file(os.path.join(self.test_dir, 'smaller_file.txt'), file_size=1024 * 60)
-        create_random_file(os.path.join(self.test_dir, 'larger_file.txt'), file_size=1024 * 6)
-        create_random_file(os.path.join(self.test_dir, 'hash_unmatch_file.txt'), file_size=1024 * 256)
+        create_random_file(Path(self.test_dir, 'fts_file.txt'))
+        create_random_file(Path(self.test_dir, 'smaller_file.txt'), file_size=1024 * 60)
+        create_random_file(Path(self.test_dir, 'larger_file.txt'), file_size=1024 * 6)
+        create_random_file(Path(self.test_dir, 'hash_unmatch_file.txt'), file_size=1024 * 256)
 
     def test_fts(self):
         with self.assertWarns((ResourceWarning, DeprecationWarning)):
-            fts = FTS(base_dir=self.test_dir, use_ssl=True, repeated=False, password='test')
+            fts = FTS(base_dir=self.test_dir, use_ssl=True, password='test')
             handle_ctrl_event(fts.logger)
             fts.start()
 
